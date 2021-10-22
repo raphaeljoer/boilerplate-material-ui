9895bc39c308f59224e1374840b6220d7587d26f
Raphael Joer <ra.joer@gmail.com>
2021-10-22 18:52:37 -0300

    Implemented base of WizardCreate layout

--- src/views/Wizard/index.tsx
+++ src/views/Wizard/index.tsx
@@ -1,29 +1,25 @@
+//material-ui
 import { Box, Grid } from '@mui/material';
+//resources
 import React from 'react';
+//core-components
 import { Layout } from '../../components';
+//styles
 import * as styles from './styles';
 
 export function Wizard() {
   return (
     <Layout>
-      <Box sx={styles.content}>
-        <Box sx={styles.box}>Wizard 01</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-        <Box sx={styles.box}>Wizard 02</Box>
-      </Box>
+      <Grid sx={styles.grid}>
+        <Box sx={styles.main}>
+          <Grid sx={styles.content.grid}>
+            <Box sx={styles.content.left}>left</Box>
+            <Box sx={styles.content.center}>center</Box>
+            <Box sx={styles.content.right}>right</Box>
+          </Grid>
+        </Box>
+        <Box sx={styles.footer}>footer</Box>
+      </Grid>
     </Layout>
   );
 }
--- src/views/Wizard/styles.ts
+++ src/views/Wizard/styles.ts
@@ -1,26 +1,43 @@
 import { Theme } from '@mui/material';
 import { SxProps } from '@mui/system';
 
-export const container: SxProps<Theme> = {
-  minHeight: '100%',
-  bgcolor: 'red'
+export const grid: SxProps<Theme> = {
+  display: 'grid',
+  height: '100%',
+  width: '100%',
+  overflow: 'scroll',
+  gridTemplateColumns: '1fr',
+  gridTemplateRows: '1fr 80px',
+  gridTemplateAreas: `"main""footer"`
 };
 
-export const content: SxProps<Theme> = {
-  height: 'calc(100%,-80px)',
+export const main: SxProps<Theme> = {
+  gridArea: 'main',
+  height: '100%',
+  width: '100%',
   overflow: 'scroll',
   maxWidth: (theme) => theme.breakpoints.values.lg,
-  mx: 'auto',
-  bgcolor: 'green'
-};
-export const box: SxProps<Theme> = {
-  bgcolor: 'orange',
-  height: '300px'
+  mx: 'auto'
 };
 
 export const footer: SxProps<Theme> = {
-  position: 'fixed',
-  bottom: 0,
-  width: '100%',
-  bgcolor: 'yellow'
+  gridArea: 'footer',
+  borderTop: 1,
+  borderColor: 'grey.300'
 };
+
+export namespace content {
+  export const grid: SxProps<Theme> = {
+    display: 'grid',
+    height: '100%',
+    width: '100%',
+    gridTemplateColumns: 'auto 1fr auto',
+    gridTemplateRows: '1fr',
+    gridTemplateAreas: `"left center right"`,
+    gap: 2
+  };
+
+  export const left: SxProps<Theme> = {};
+  export const center: SxProps<Theme> = {};
+  export const right: SxProps<Theme> = {};
+}
