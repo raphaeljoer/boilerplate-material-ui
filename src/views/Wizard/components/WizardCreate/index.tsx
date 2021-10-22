c2d352e99ac2f77162167fa5389341dfa13d3ee1
Raphael Joer <ra.joer@gmail.com>
2021-10-22 20:41:21 -0300

    Implemented Tips data layer

--- src/views/Wizard/components/WizardCreate/index.tsx
+++ src/views/Wizard/components/WizardCreate/index.tsx
@@ -7,22 +7,22 @@ import React from 'react';
 import { Section, Tip, Welcome } from './components';
 //styles
 import * as styles from './styles';
+//types
+import { ITip } from './types/tip';
 
 type Props = {
   sx?: SxProps<Theme>;
+  tips: ITip[];
 };
 
-export function WizardCreate({ sx }: Props) {
+export function WizardCreate({ tips, sx }: Props) {
   return (
     <Grid sx={{ ...styles.grid, ...sx }}>
       <Box sx={styles.main}>
         <Grid sx={styles.content.grid}>
           <Box sx={styles.content.left}>
             <Welcome />
-            <Tip
-              tips={[{ title: 'Welcome', description: 'Description' }]}
-              activeStep={0}
-            />
+            <Tip tips={tips} activeStep={0} />
           </Box>
           <Box sx={styles.content.main}>
             <Section title="title" description="description">
--- src/views/Wizard/index.tsx
+++ src/views/Wizard/index.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 //core-components
 import { Layout } from '../../components';
 import { WizardCreate } from './components/WizardCreate';
+import { tips } from './components/WizardCreate/data/tips';
 
 export function Wizard() {
   return (
     <Layout>
-      <WizardCreate sx={{ bgcolor: 'grey.100' }} />
+      <WizardCreate sx={{ bgcolor: 'grey.100' }} tips={tips} />
     </Layout>
   );
 }
